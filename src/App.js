753918b994fefa6e9ddea26ea33423554b182003
mguidoti <marcus.guidoti@gmail.com>
2020-11-03 22:40:56 -0300

    feat: Add Create and Remove to/from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
 
+// Load the api script, with the axios object, connecting to the API
+import api from './services/api';
+
 function App() {
+
+  // Defines the array that will hold the data
+  const [repos, setRepos] = useState([]);
+  
+  // Define the state-watcher kind of thing
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepos(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    
+    const response = await api.post('/repositories', {
+      title: `New Repo ${Date.now()}`,
+      url: "http://www.github.com//mguidoti/fakerepo",
+      techs: "React"
+    });
+
+    const repo = response.data;
+
+    setRepos([...repos, repo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
+    const repoIndex = repos.findIndex(repo => repo.id === id);
+
+    await api.delete(`/repositories/${id}`);
+
+    repos.splice(repoIndex, 1);
+
+    setRepos([...repos]);
+    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        { repos.map((repo) => ( 
+            <>
+              <li>
+                <h2 key={repo.id}> { repo.title } </h2> 
+                <button onClick={() => handleRemoveRepository(repo.id)}>
+                  Remover
+                </button>
+              </li>
+            </>
+            ) ) }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
